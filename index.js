a6b778d3fb464486005892e682a812cafa5fcff1
Irwin Billing <irwin.billing@gmail.com>
2017-05-14 14:20:28 -0700

    adding support for update twitch message after streaming is done

--- index.js
+++ index.js
@@ -3,76 +3,111 @@ const Members = require('members');
 const SlackClient = require('slack-client');
 const Promise = require('bluebird');
 
+const GamingSlackChannelId = 'C23QJCUET';
+
 module.exports = class TwitchBot {
   constructor(membersConnectionString, slackToken, twitchClientId) {
-    this.currentlyStreaming = [];
-    this.members = new Members(membersConnectionString);
+    this.membersClient = new Members(membersConnectionString);
     this.slackClient = new SlackClient(slackToken);
     this.twitchClientId = twitchClientId;
   }
 
-  poll() {
-    let twitchToMember = {};
-    console.log("getting members");
-    return this.members
-      .getMembers()
-      .then(result => {
-        console.log(`got ${result.count} members`);
-        const members = result
-          .filter(member => member.TwitchId && member.TwitchId.length > 0);
+  sendSlackMessage(message, twitchStream) {
+    return this.slackClient
+      .sendMessage(
+        GamingSlackChannelId,
+        message,
+        [{
+          title: twitchStream.channel.status,
+          text: twitchStream.channel.url,
+          image_url: twitchStream.preview.medium,
+          thumb_url: twitchStream.preview.small
+        }]);
+  }
 
-        twitchToMember = members.reduce((acc, member) => {
+  sendSlackUpdate(message, messageId, twitchStream) {
+    return this.slackClient.updateMessage(
+      GamingSlackChannelId,
+      messageId,
+      message,
+      [{
+        title: twitchStream.channel.status,
+        text: twitchStream.channel.url,
+        image_url: twitchStream.preview.medium,
+        thumb_url: twitchStream.preview.small
+      }]);
+  }
+
+  *poll() {
+    let twitchToMember = {};
+    if (!this.members) {
+      const response = yield this.membersClient.getMembers();
+      this.members = response
+        .filter(member => member.TwitchId && member.TwitchId.length > 0)
+        .map(member => {
+          member.isStreaming = false;
+          member.slackMessageId = null;
+          return member;
+        })
+      this.twitchIdToMember = this.members.reduce((acc, member) => {
           acc[member.TwitchId] = member;
           return acc;
         }, {});
+    }
 
-        return members
-          .map(member => member.TwitchId)
-          .join(',');
-      })
-      .then(result => {
-        console.log(`getting status for users with twitch IDs ${result}`);
-        return rp({
-            method: 'GET',
-            uri: `https://api.twitch.tv/kraken/streams?channel=${result}`,
-            headers: {
-              'Client-ID': this.twitchClientId,
-              'Accept': 'application/vnd.twitchtv.v5+json'
-            },
-            json: true,
-          });
-      })
-      .then(result => {
-        if (!result || !result.streams || result.streams.length === 0) {
-          console.log(`noone is streaming: ${result}`);
-          return Promise.resolve();
+    const twitchIdCsv = this.members
+      .map(member => member.TwitchId)
+      .join(',');
+
+    const twitchResponse = yield rp({
+      method: 'GET',
+      uri: `https://api.twitch.tv/kraken/streams?channel=${twitchIdCsv}`,
+      headers: {
+        'Client-ID': this.twitchClientId,
+        'Accept': 'application/vnd.twitchtv.v5+json'
+      },
+      json: true,
+    });
+
+    if (!twitchResponse) {
+      twitchResponse = {
+        streams: []
+      };
+    } else if (!twitchResponse.streams) {
+      twitchResponse.streams = [];
+    }
+
+    console.log(`${twitchResponse.streams.length} people are streaming`);
+
+    return this.members.map(member => {
+      const twitchStream = twitchResponse.streams.find(s => s.channel._id == member.TwitchId);
+
+      if (twitchStream) {
+        if (member.isStreaming) {
+          return this.sendSlackUpdate(
+            `<@${member.SlackId}> is streaming ${twitchStream.game}!   :twitch_live: LIVE`,
+            member.slackMessageId,
+            twitchStream);
+        } else {
+          member.isStreaming = true;
+          member.stream = twitchStream;
+          return this.sendSlackMessage(
+              `<@${member.SlackId}> is streaming ${twitchStream.game}!   :twitch_live: LIVE`,
+              twitchStream)
+            .then(slackResponse => {
+              member.slackMessageId = slackResponse.ts;
+            });
         }
-
-        console.log(`${result.streams.length} people are streaming`);
-        const newStreamers = [];
-
-        this.currentlyStreaming = result.streams.map(stream => {
-          const id = stream.channel._id;
-
-          if (!this.currentlyStreaming.includes(id)) {
-            newStreamers.push(stream);
-          }
-
-          return id;
-        });
-
-        return Promise.all(newStreamers.map(stream => {
-          const member = twitchToMember[stream.channel._id];
-
-          return this.slackClient.sendMessage('C23QJCUET',
-            `<@${member.SlackId}> started streaming ${stream.game}!`,
-            [{
-              title: stream.channel.status,
-              text: stream.channel.url,
-              image_url: stream.preview.medium,
-              thumb_url: stream.preview.small
-            }]);
-        }));
-      });
+      } else {
+        if (member.isStreaming) {
+          member.isStreaming = false;
+          return this.sendSlackUpdate(
+            `<@${member.SlackId}> was streaming ${member.stream.game}.`,
+            member.slackMessageId,
+            member.stream);
+        }
+      }
+      return Promise.resolve();
+    });
   }
 }
